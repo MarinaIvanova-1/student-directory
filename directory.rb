dcb60da68d7929f408d2ba06a451d30b6f62b87e
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-08-14 17:16:11 +0100

    File save functionality added

--- directory.rb
+++ directory.rb
@@ -7,30 +7,26 @@ def interactive_menu
   end
 end  
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to studetns.csv"
+  puts "9. Exit"
+end
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else 
       puts "I don't know what you meant, please try again"
   end
-end
-
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
 end
 
 def input_students
@@ -48,6 +44,12 @@ def input_students
   end
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "------"
@@ -61,6 +63,18 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  #open the file for writing
+  file = File.open("students.csv", "w")
+  #iterate over the array of students 
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
