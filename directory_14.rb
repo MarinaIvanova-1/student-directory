33cf35506b69f0defc7a657a8a43a386c15918ec
Marina Ivanova <ubcg62bs@Marinas-MacBook-Pro.local>
2022-08-14 21:26:26 +0100

    Refactore with File do...end function in all methods

--- directory_14.rb
+++ directory_14.rb
@@ -76,15 +76,16 @@ end
 def save_students
   #open the file for writing
   puts "Please enter the name of the file to save"
-  filename = gets.chomp
-  file = File.open(filename, "w")
-  #iterate over the array of students 
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  filename = STDIN.gets.chomp
+  File.open(filename, "w") do |file|
+    #iterate over the array of students 
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
+  
 end
 
 def load_students(filename = "students.csv")
